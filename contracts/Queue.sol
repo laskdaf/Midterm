71f7e11364e48630bcd94d4d7b9de9d3c5a663e3
Bunagana <melbinger2008@gmail.com>
2017-10-28 15:31:10 -0700

    Merge

--- contracts/Queue.sol
+++ contracts/Queue.sol
@@ -8,52 +8,5 @@ pragma solidity ^0.4.15;
  */
 
 contract Queue {
-	/* State variables */
-	uint8 size = 5;
-	// YOUR CODE HERE
-
-	/* Add events */
-	// YOUR CODE HERE
-
-	/* Add constructor */
-	// YOUR CODE HERE
-
-	/* Returns the number of people waiting in line */
-	function qsize() constant returns(uint8) {
-		// YOUR CODE HERE
-	}
-
-	/* Returns whether the queue is empty or not */
-	function empty() constant returns(bool) {
-		// YOUR CODE HERE
-	}
 	
-	/* Returns the address of the person in the front of the queue */
-	function getFirst() constant returns(address) {
-		// YOUR CODE HERE
-	}
-	
-	/* Allows `msg.sender` to check their position in the queue */
-	function checkPlace() constant returns(uint8) {
-		// YOUR CODE HERE
-	}
-	
-	/* Allows anyone to expel the first person in line if their time
-	 * limit is up
-	 */
-	function checkTime() {
-		// YOUR CODE HERE
-	}
-	
-	/* Removes the first person in line; either when their time is up or when
-	 * they are done with their purchase
-	 */
-	function dequeue() {
-		// YOUR CODE HERE
-	}
-
-	/* Places `addr` in the first empty position in the queue */
-	function enqueue(address addr) {
-		// YOUR CODE HERE
-	}
 }
